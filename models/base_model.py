ebd0ac94b199e1748b1d9a377584dae2b7cf8b27
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2023-12-09 18:19:46 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,9 @@ import datetime
 class BaseModel():
     """the basic class."""
     def __init__(self):
-        self.id = id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
